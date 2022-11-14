afdf827fc4eb0b884b7b2881e8ee6dd0d1cf9973
Leizar06001 <110201367+Leizar06001@users.noreply.github.com>
2022-11-14 21:54:07 +0100

    up

--- main.c
+++ main.c
@@ -20,11 +20,15 @@ struct poke
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 char **create_db();
 void free_db(char **data);
 int	fill_db(struct poke *lst_pkm);
 int	select_pkm(struct poke *data, int pkmn_nbr);
+void print_pokemon(int n_pokemon);
+void my_delay(int i);
 
 void read_csv(int row, int col, char *filename, double **data);
 /*
@@ -74,23 +78,39 @@ int main(int argc, char const *argv[])
 	//compile_error();
 	//logo(argv[1]);
 	system("clear");
-	printf("Lecture de la base de données...\n");
+	write(1, "Lecture de la base de donnees", 29);
+	for (int i = 0; i < 3; i ++)
+	{
+		write(1, ".", 1);
+		my_delay(700);
+	}
 	//pkm_db = create_db();
 	
 	nb_pkm = fill_db(lst_pkm);
-
-	printf("%d pokémons enregistrés\n\n\n", nb_pkm);
-	printf("*** Bienvenue sur le Pokedex ***\nEntrez un numero de Pokemon\n> ");
+	printf("\n");
+	printf("    %d pokémons enregistrés\n\n\n", nb_pkm);
+	my_delay(2000);
 
 	pkmn_nbr = 1;
 	while (pkmn_nbr != 0)
 	{
+		system("clear");
+		printf("*** Bienvenue sur le Pokedex ***\nEntrez un numero de Pokemon\n> ");
+
 		scanf("%d", &pkmn_nbr);
-		select_pkm(lst_pkm, pkmn_nbr);
+		system("clear");
+		if (pkmn_nbr != 0)
+		{
+			select_pkm(lst_pkm, pkmn_nbr);
+			print_pokemon(pkmn_nbr);
+			printf("\n\n\n\nAppuyez sur entree pour continuer");
+			getchar();
+			getchar();
+		}
 	}
 	//free_db(pkm_db);
 
-	for (int i = 0; i < nb_of_poke; i++)
+	for (int i = 0; i < nb_of_poke - 1; i++)
 	{
 		free(lst_pkm[i].name);
 		free(lst_pkm[i].type1);
@@ -99,4 +119,11 @@ int main(int argc, char const *argv[])
 	free(lst_pkm);
 	
 	return 0;
+}
+
+void my_delay(int i)    /*Pause l'application pour i ms*/
+{
+    clock_t start,end;
+    start=clock();
+    while(((end=clock())-start)<=((i*CLOCKS_PER_SEC)/1000));
 }
